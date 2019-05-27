972de914ad7ccc9979552c8d9f410e4a4eff833d
Lou-Bloom <36960884+Lou-Bloom@users.noreply.github.com>
2019-05-27 02:59:09 +0100

    Add files via upload
    Stefan_Wojcik_P17213769

---
+++ libs.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <glfw3.h>
+#include <glm.hpp>
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat4x4.hpp>
+#include <gtc\matrix_transform.hpp>
+#include <gtc\type_ptr.hpp>
+//#include <SOTL2.h>
+
+struct Vertex
+{
+	glm::vec3 position;
+	glm::vec3 color;
+	glm::vec2 texcoord;
+	glm::vec3 normal;
+};
